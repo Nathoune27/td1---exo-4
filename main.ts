4c87800eda09fcc989670ac4c940e510383d8b68
Nathoune27 <68045427+Nathoune27@users.noreply.github.com>
2020-07-15 10:21:02 +1100

    Mise à jour main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,26 +1,26 @@
 input.onButtonPressed(Button.A, function () {
-    if (x == -1 && y > -1) {
-        led.unplot(x, y)
-        x += 5
+    led.unplot(x, y)
+    x += -1
+    if (x == -1 && y == 0) {
+        x = 4
+        y = 4
+    } else if (x == -1 && y > 0) {
+        x = 4
         y += -1
-        led.plot(x, y)
-    } else if (x > -1 && y > -1) {
-        led.unplot(x, y)
-        x += -1
-        led.plot(x, y)
     }
+    led.plot(x, y)
 })
 input.onButtonPressed(Button.B, function () {
-    if (x == 5 && y < 5) {
-        led.unplot(x, y)
-        x += -5
+    led.unplot(x, y)
+    x += 1
+    if (x == 5 && y == 4) {
+        x = 0
+        y = 0
+    } else if (x == 5 && y < 4) {
+        x = 0
         y += 1
-        led.plot(x, y)
-    } else if (x < 5 && y < 5) {
-        led.unplot(x, y)
-        x += 1
-        led.plot(x, y)
     }
+    led.plot(x, y)
 })
 let y = 0
 let x = 0
